fa9a9caea1bc83d652283dc05759ada630bfc5f8
Vlad Sobchuk <vlad.sobchuk@itechart-group.com>
2022-07-19 11:57:53 +0300

    Initial Commit

---
+++ test-firestore/test_firestoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_firestoreApp.swift
+//  test-firestore
+//
+//  Created by Vlad Sobchuk on 7/19/22.
+//
+
+import SwiftUI
+
+@main
+struct test_firestoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
